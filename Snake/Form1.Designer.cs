d7419ffbf48583c57d0c49b2453d38e651c3f264
Michal Sekyra <Sekyra.Michal@stud.vassboskovice.cz>
2021-10-19 13:09:01 +0200

    Revert "Revert "Generování hadů""
    This reverts commit d4c3565fe6601f627808463fe2cf724736ef4d2e.

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(734, 711);
+            this.IsMdiContainer = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(750, 750);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(750, 750);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -12,10 +12,29 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+        PictureBox had1;
+        PictureBox had2;
+
         public Form1()
         {
             InitializeComponent();
-        }
+            VytvorHady();
 
+        }
+        private void VytvorHady()
+        {
+            had1 = new PictureBox();
+            had1.Size = new Size(40,40);
+            had1.Location = new Point(10, 10);
+            had1.BackColor = Color.LimeGreen;
+            this.Controls.Add(had1);
+
+            had2 = new PictureBox();
+            had2.Size = new Size(40, 40);
+            had2.Location = new Point(ClientSize.Width-60,10);
+            had2.BackColor = Color.Red;
+            this.Controls.Add(had2);
+        }
     }
+
 }
